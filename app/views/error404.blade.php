d9383b58dd6368d2022c25290f221c72225b1e92
etomarat <etomarat@gmail.com>
2015-01-16 17:27:56 +0300

    404 styles

--- app/views/error404.blade.php
+++ app/views/error404.blade.php
@@ -6,12 +6,16 @@
 
 
 @section('content')
-
+    <div class="r404-wrapper">
+        <div class="h"></div>
         <div class="r404">
+            <h2 class="slide-head" style="margin-bottom: 0;">
+                ОШИБКА 404
+            </h2>
             Запрашиваемая вами страница не существует.<br>
             Попробуйте начать с <a href="/">главной страницы</a>.
         </div>
-
+    </div>
 @stop
 
 
